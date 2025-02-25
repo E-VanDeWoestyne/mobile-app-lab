2177e0bd27b78cf1082109442ce333743705f29a
Ekamjot Singh <160436305+ekam-brar-1@users.noreply.github.com>
2025-02-25 11:30:17 -0700

    created list

--- app/lab4.tsx
+++ app/lab4.tsx
@@ -1,1 +1,20 @@
-import vacationDestinations from "./constants/list_items";
+import { ScrollView, View, Text } from "react-native";
+import vacationDestinations from "../constants/list_items";
+export default function Lab4() {
+  return (
+    <ScrollView>
+      <Text>
+        {" "}
+        <h1>Choose your destinations</h1>
+      </Text>
+      {vacationDestinations.map((destination) => (
+        <View key={destination.id}>
+          <Text>{"\u2705"}</Text>
+          <Text>{destination.location}</Text>
+          <Text>{destination.price}</Text>
+          <Text>{destination.average_yearly_temperature}</Text>
+        </View>
+      ))}
+    </ScrollView>
+  );
+}
