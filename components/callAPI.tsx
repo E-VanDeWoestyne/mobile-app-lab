7a8efa7b0fe0dd4c85a069ad342272e93e562874
Ekamjot Singh <160436305+ekam-brar-1@users.noreply.github.com>
2025-03-04 11:55:05 -0700

    changed fetch

--- components/callAPI.tsx
+++ components/callAPI.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { View, Button, StyleSheet } from "react-native";
+import { View, Button, Text, StyleSheet } from "react-native";
 
 export default function callAPI() {
   const [response, setResponse] = useState(null);
-  useEffect(async () => {
-    try {
-      let response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts/1"
-      );
-      let json = await response.json();
-      console.log(json);
-    } catch (error) {
-      console.error(error);
-    }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts/1"
+        );
+        setResponse(await response.json());
+        console.log(setResponse);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Button title="Call API" onPress={() => console.log(response)} />
+      <Text>{response}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
