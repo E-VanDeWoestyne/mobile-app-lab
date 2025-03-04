f9d93a7c4e32b2416557a0eec3a93d566b736d13
Ekamjot Singh <160436305+ekam-brar-1@users.noreply.github.com>
2025-03-04 11:48:13 -0700

    added async

--- components/callAPI.tsx
+++ components/callAPI.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { View, Button, StyleSheet } from "react-native";
 
 export default function callAPI() {
+  const [response, setResponse] = useState(null);
+  useEffect(async () => {
+    try {
+      let response = await fetch("https://jsonplaceholder.typicode.com/posts");
+      let json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
   return <View style={styles.container}></View>;
 }
 
